340abfc9de9209a9d3fa29fd1cd199a7076d5b9a
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2025-02-01 14:38:44 -0500

    drug names

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from ucimlrepo import fetch_ucirepo 
 
 app = Flask(__name__)
 CORS(app)
 
+drug_reviews_druglib_com = fetch_ucirepo(id=461) 
+data = drug_reviews_druglib_com.data.features
+
+
 @app.route('/api/drugs', methods=['GET'])
-def get_drugs():
-    drug_reviews_druglib_com = fetch_ucirepo(id=461) 
-    X = drug_reviews_druglib_com.data.features
-    y = drug_reviews_druglib_com.data.targets
-    return jsonify({'X': X, 'y': y})
+def get_drugs():    
+    drugs = data['urlDrugName'].unique().tolist()
+    return jsonify(drugs)
 
 
 if __name__ == '__main__':
