d03a5870b187c134fa08e6d1f6cce7a4b13f7f44
Erica Dong <erica3280cell@gmail.com>
2025-02-01 17:58:08 -0500

    add new review creation

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from ucimlrepo import fetch_ucirepo
 from pymongo.mongo_client import MongoClient
 from bson import json_util
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -22,7 +23,7 @@ def get_drugs():
     drugs = data['urlDrugName'].unique().tolist()
     return jsonify(drugs)
 
-@app.route('/api/drug-info/', methods=['GET'])
+@app.route('/api/drugs/info', methods=['GET'])
 def get_drug_info():
     drug_name = request.args.get('drug')
     drug = drugs.find_one({"name": drug_name}, {"_id": 0})
@@ -30,6 +31,22 @@ def get_drug_info():
         return jsonify({"error": "Drug not found"}), 404
 
     return Response(json_util.dumps(drug), mimetype='application/json')
+
+@app.route('/api/drugs/new-review', methods=['POST'])
+def post_drug_review():
+    data = request.get_json()
+    drug_name = data.get('drug')
+    review = data.get('review')
+    review["created"] = datetime.now()
+
+    drug = drugs.find_one({"name": drug_name})
+    if not drug:
+        drugs.insert_one({"name": drug_name, "reviews": [review]})
+    else:   
+        drug['reviews'].append(review)
+        drugs.update_one({"name": drug_name}, {"$set": {"reviews": drug['reviews']}})
+
+    return jsonify(review)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
