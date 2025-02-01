114997cdb975c9549860cce354d93e24dacc6c3f
Isha Nagireddy <nagireddyisha@gmail.com>
2025-02-01 17:46:33 -0500

    added dropdown

--- frontend/rate-my-drug/app/searchbar.tsx
+++ frontend/rate-my-drug/app/searchbar.tsx
@@ -1,26 +1,40 @@
-'use client';
+"use client";
 import React, { useState } from 'react';
-  export const SearchBar = () => {
-    const [searchInput, setSearchInput] = useState("");
-    const drugs = ["Vicodin", "Oxycontin", "Adderall", "Xanax", "Cocaine", "Heroin", "Meth", "LSD", "Shrooms", "Weed"];
-  
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setSearchInput(e.target.value);
-    };
-  
-    if (searchInput.length > 0) {
-      drugs.filter((drugs) => {
-      return drugs.match(searchInput);
-  });
-  }
+import Select from 'react-select';
+const optionsArray = [
+  { value: "Cannabis", label: "Cannabis" },
+  { value: "Cocaine", label: "Cocaine" },
+  { value: "Ecstasy", label: "Ecstasy" },
+  { value: "Heroin", label: "Heroin" },
+  { value: "Ketamine", label: "Ketamine" },
+  { value: "LSD", label: "LSD" },
+  { value: "Methamphetamine", label: "Methamphetamine" },
+  { value: "Mushrooms", label: "Mushrooms" },
+  { value: "Prescription Drugs", label: "Prescription Drugs" },
+  { value: "Steroids", label: "Steroids" },
+  { value: "Tobacco", label: "Tobacco" },
+  { value: "Other", label: "Other" }
+];
 
-  return <div>
-    <input
-            type="text"
-            placeholder="Search for a drug..."
-            onChange={handleChange}
-            value={searchInput}
-            className="border-2 border-gray-300 rounded-lg p-6 w-full placeholder:text-lg"/>
-  </div>
-}
+const SearchDropdown = () => {
+  const [selectedOption, setSelectedOption] = useState<{ value: string; label: string } | null>(null);
+
+  const handleChange = (option: { value: string; label: string } | null) => {
+    setSelectedOption(option);
+    console.log(`Option selected:`, option);
+  };
+  
+
+  return (
+    <div>
+      <Select className='"border-2 border-gray-300 rounded-lg p-6 w-full placeholder:text-lg"'
+        value={selectedOption}
+        placeholder="Search for a drug..."
+        onChange={handleChange}
+        styles={{ container: (provided) => ({ ...provided, width: 500 }) }}
+        options={optionsArray}/>
+    </div>
+  );
+};
+
+export default SearchDropdown;
