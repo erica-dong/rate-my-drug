5c305ad9767bbad6ebc9dc185ea52e6034d594ae
Erica Dong <erica3280cell@gmail.com>
2025-02-01 17:48:07 -0500

    add drug info fetch

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 from ucimlrepo import fetch_ucirepo
 from pymongo.mongo_client import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,9 @@ CORS(app)
 uri = 'mongodb+srv://armaanp4423:tHlk2dWxoH4aZRn8@rmdcluster.yhag7.mongodb.net/?retryWrites=true&w=majority&appName=rmdcluster'
 client = MongoClient(uri)
 client.list_database_names()
+
+db = client["drugs"]
+drugs = db["drugs"]
 
 @app.route('/api/drugs', methods=['GET'])
 def get_drugs():
@@ -18,6 +22,14 @@ def get_drugs():
     drugs = data['urlDrugName'].unique().tolist()
     return jsonify(drugs)
 
+@app.route('/api/drug-info/', methods=['GET'])
+def get_drug_info():
+    drug_name = request.args.get('drug')
+    drug = drugs.find_one({"name": drug_name}, {"_id": 0})
+    if not drug:
+        return jsonify({"error": "Drug not found"}), 404
+
+    return Response(json_util.dumps(drug), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
